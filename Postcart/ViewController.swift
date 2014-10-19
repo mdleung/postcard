0de5eee20d1aa25b2e61090cf939e8e2c69a1683
mdleung <mdleung@shaw.ca>
2014-10-19 10:03:15 -0700

    Initial Commit

---
+++ Postcart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Postcart
+//
+//  Created by Matt Leung on 2014-10-19.
+//  Copyright (c) 2014 Matt Leung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
